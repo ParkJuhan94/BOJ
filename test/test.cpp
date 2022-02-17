a7acb6a68e8aeab5e9e46711bc0bec4fd4bad207
JuHan <1004sunye@naver.com>
2022-02-17 22:40:57 +0900

    0217-01

--- test/test.cpp
+++ test/test.cpp
@@ -1,22 +1,61 @@
-/*
-https://www.acmicpc.net/problem/10872
-*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int factorial(int n) {
-	if (n == 0) {
-		return 1;
-	}
-	else {
-		return n * factorial(n - 1);
-	}
-}
-
-
 int main() {
-	int N;
-	scanf("%d", &N);
+    int a, b, c;
+    int flagA = 0, flagB = 0, flagC = 0;
+    int max = 0;
+    int equal_3 = 0, equal_2 = 0;
+    int res = 0;
+    scanf("%d %d %d", &a, &b, &c);
 
-	printf("%d\n", factorial(N));
+    if (a > max) {
+        max = a;
+    }
+    if (b > max) {
+        max = b;
+    }
+    if (c > max) {
+        max = c;
+    }
+
+
+    if (a == b && b == c) {
+        equal_3 = 1;
+    }
+    else if (a == b) {
+        equal_2 = 1;
+        flagA = 1;
+        flagB = 1;
+    }
+    else if (b == c) {
+        equal_2 = 1;
+        flagB = 1;
+        flagC = 1;
+    }
+    else if (c == a) {
+        equal_2 = 1;
+        flagC = 1;
+        flagA = 1;
+    }
+    //printf("cnt : %d\n", cnt);
+
+    if (equal_3 == 1) {
+        res = 10000 + a * 1000;
+    }
+    if (equal_3 != 1 && equal_2 == 1) {
+        if (flagA == 1) {
+            res = 1000 + a * 100;
+        }
+        if (flagB == 1) {
+            res = 1000 + b * 100;
+        }
+        if (flagC == 1) {
+            res = 1000 + c * 100;
+        }
+    }
+    if (equal_3 != 1 && equal_2 != 1) {
+        res = max * 100;
+    }       
+    printf("%d\n", res);
 }
