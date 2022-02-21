ecad97fbb2f8eeca05c3621af9d76b1a675e145a
JuHan <1004sunye@naver.com>
2022-02-22 03:46:38 +0900

    0222-01

--- test/test.cpp
+++ test/test.cpp
@@ -1,47 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int isPrimeNum(int n) {
-    int cnt = 0;
-
-    if (n == 2) {
-        return 1;
-    }
-
-    for (int i = 1; i * i <= n; i++) {
-        if (n % i == 0) {
-            cnt++;
-        }
-        if (cnt > 1) {
-            return 0;
-        }
-    }
-
-    if (cnt == 1) {
-        return 1;
-    }
-}
-
 int main() {
-    int i, j, k;
-    int n, evenNum;
-    int max = 0, min = 10000;
-    
+    int a[4];
+    int i, j;
+    int sum = 0;
 
-    scanf("%d", &n);
-    for (i = 0; i < n;i++) {
-        scanf("%d", &evenNum);
-        max = 0;
-        min = 10000;
+    for (i = 0; i < 3; i++) {
+        sum = 0;
+        scanf("%d %d %d %d", &a[0], &a[1], &a[2], &a[3]);
+        
 
-        for (j = 2; j <= evenNum / 2; j++) {
-            if (isPrimeNum(j) && isPrimeNum(evenNum - j)) {
-                if (j > max) {
-                    max = j;
-                }
-            }
+        for (j = 0; j < 4; j++) {
+            sum += a[j];
         }
-
-        printf("%d %d\n", max, evenNum - max);
-    }    
+        if(sum == 3){
+            printf("A\n");
+        }
+        else if (sum == 2) {
+            printf("B\n");
+        }
+        else if (sum == 1) {
+            printf("C\n");
+        }
+        else if (sum == 4) {
+            printf("E\n");
+        }
+        else if (sum == 0) {
+            printf("D\n");
+        }
+    }
 }
--- test_for_c/test.cpp
+++ test_for_c/test.cpp
@@ -1,31 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-/*
-a는 첫 항이고, b는 두 번째 항이다.
-처음에 둘 다 1로 시작하므로 마지막 역시 a와 b가 1이 된다면 한 주기가 완성된 것이다.
-m으로 나눈 나머지에 대해서만 고려하면 되므로 a+b를 하고 항상 %m을 해준다.
-*/
 int main() {
-    int P, N, M;
-    scanf("%d", &P);
+    int n;
+    scanf("%d", &n);
 
-    int cnt, a, b;
-    for (int i = 0; i < P; i++) {
-        scanf("%d %d", &N, &M);
-        cnt = 0;
-        a = b = 1;
+    int i;
+    int comma;
+    int a, b;
 
-        while (1) {
-            int tmp = (a + b) % M;
-            a = b;
-            b = tmp;
-            cnt++;
-
-            //주기를 찾으면 break
-            if (a == 1 && b == 1) break;
-        }
-        printf("%d %d\n", N, cnt);
+    for (i = 0; i < n; i++) {
+        scanf("%d %d %d", &a, &comma, &b);
+        printf("%d\n", a + b);
     }
-    return 0;
 }
