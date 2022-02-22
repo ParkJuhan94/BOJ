ea96c2dece95a1a8ce55ff967a2103a27a5ce838
JuHan <1004sunye@naver.com>
2022-02-22 19:19:45 +0900

    0222

--- test/test.cpp
+++ test/test.cpp
@@ -1,33 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    int a[4];
-    int i, j;
-    int sum = 0;
+	int map[8][8];
+	int i, j;
+	int cnt = 0;
 
-    for (i = 0; i < 3; i++) {
-        sum = 0;
-        scanf("%d %d %d %d", &a[0], &a[1], &a[2], &a[3]);
-        
-
-        for (j = 0; j < 4; j++) {
-            sum += a[j];
-        }
-        if(sum == 3){
-            printf("A\n");
-        }
-        else if (sum == 2) {
-            printf("B\n");
-        }
-        else if (sum == 1) {
-            printf("C\n");
-        }
-        else if (sum == 4) {
-            printf("E\n");
-        }
-        else if (sum == 0) {
-            printf("D\n");
-        }
-    }
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+			scanf("%c", &map[i][j]);
+			if (map[i][j] == 'F' && (i + j) % 2 == 0) {
+				cnt++;
+			}
+		}
+	}
+	printf("%d", cnt);
 }
